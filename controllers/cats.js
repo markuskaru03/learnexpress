30b34a064a7ac20761a4d9bbd3245e5102def517
Markus Karu <markus.max.karu@gmail.com>
2026-03-11 11:43:25 +0200

    Add pagination to list

--- controllers/cats.js
+++ controllers/cats.js
@@ -6,8 +6,16 @@ import { prisma } from "../lib/prisma.js";
 const router = express.Router();
 
 router.get('', async (req, res) => {
-  let cats = await prisma.cat.findMany();
-  res.render('cats/index.njk', {cats});
+  let count = await prisma.cat.count();
+  let perPage = 10;
+  let pages = Math.ceil(count / perPage);
+  let currentPage = parseInt(req.query.page ?? 1);
+  let skip = perPage * (currentPage-1);
+  let cats = await prisma.cat.findMany({
+    take: perPage,
+    skip: skip,
+  });
+  res.render('cats/index.njk', { cats, pages });
 });
 
 router.get('/create', (req, res) => {
